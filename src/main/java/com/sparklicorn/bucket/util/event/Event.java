0c067cfb10a61034262765471f2118dc8f28199d
Jeff <sparklicorn@users.noreply.github.com>
2022-06-12 18:25:36 -0500

    Add tests for Event

--- src/main/java/com/sparklicorn/bucket/util/event/Event.java
+++ src/main/java/com/sparklicorn/bucket/util/event/Event.java
@@ -2,7 +2,6 @@ package com.sparklicorn.bucket.util.event;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 
 /**
  * Events contain information about to some system event, including
@@ -14,18 +13,33 @@ import java.util.Map.Entry;
  */
 public class Event {
 
+	public static final String NAME_PATTERN = "[a-zA-Z][a-zA-Z_0-9\\-]*";
+
 	/**
-	 * A RuntimeException that is thrown when attempting to add properties
+	 * A RuntimeException thrown when attempting to add properties
 	 * to a frozen Event.
 	 */
 	public static final class EventFrozenException extends RuntimeException {
-		public EventFrozenException() {
-			super();
-		}
+		public static final String CANNOT_ADD_PROPERTY = "Cannot add property to frozen Event.";
 
-		public EventFrozenException(String message) {
-			super(message);
-		}
+		public EventFrozenException() { super(); }
+		public EventFrozenException(String message) { super(message); }
+	}
+
+	/**
+	 * A RuntimeException thrown when the provided event name or property name
+	 * does not conform to the specified pattern.
+	 */
+	public static final class MalformedNameException extends RuntimeException {
+		static final String BAD_NAME = """
+			Name must start with a letter and may only contain
+			lower and uppercase letters, numbers, and characters \"-_\"
+		""";
+		static final String BAD_EVENT_NAME = "Bad Event name: " + BAD_NAME;
+		static final String BAD_PROPERTY_NAME = "Bad Event property name: " + BAD_NAME;
+
+		public MalformedNameException() { super(); }
+		public MalformedNameException(String message) { super(message); }
 	}
 
 	public final String name;
@@ -36,7 +50,8 @@ public class Event {
 
 	/**
 	 * Creates a new Event with the given name.
-	 * @param name - Name of the event. This cannot be changed later.
+	 *
+	 * @param name - Name of the event. Cannot be null or empty.
 	 */
 	public Event(String name) {
 		this(name, null);
@@ -45,28 +60,31 @@ public class Event {
 	/**
 	 * Creates a new Event with the given name and copies the given properties
 	 * into the event's properties map.
-	 * @param name - Name of the event. This cannot be changed later.
-	 * @param properties - Properties to set on the new event. Can be null.
+	 *
+	 * @param name - Name of the event. Cannot be null or empty.
+	 * @param properties - Properties to set on the event.
 	 */
 	public Event(String name, Map<String,Object> properties) {
+		if(name == null) {
+			throw new NullPointerException("Name must not be null");
+		}
+
+		if (!name.matches(Event.NAME_PATTERN)) {
+			throw new MalformedNameException(MalformedNameException.BAD_EVENT_NAME);
+		}
+
 		this.name = name;
 		this.frozen = false;
 		this.properties = new HashMap<>();
 
 		if (properties != null) {
-			shallowCopyProperties(properties);
-		}
-	}
-
-	// Copies entries from the given map to this event's properties.
-	private void shallowCopyProperties(Map<String,Object> properties) {
-		for (Entry<String,Object> p : properties.entrySet()) {
-			this.properties.put(p.getKey(), p.getValue());
+			properties.forEach((propName, value) -> this.addProperty(propName, value));
 		}
 	}
 
 	/**
-	 * Sets the event to a frozen state. A frozen event cannot add properties.
+	 * Sets the event to a frozen state.
+	 * A frozen event cannot add properties and cannot be unfrozen.
 	 */
 	public void freeze() {
 		this.frozen = true;
@@ -74,6 +92,7 @@ public class Event {
 
 	/**
 	 * Whether this event is in a frozen state. A frozen event cannot add properties.
+	 *
 	 * @return True if the event is frozen; otherwise false.
 	 */
 	public boolean isFrozen() {
@@ -82,34 +101,43 @@ public class Event {
 
 	/**
 	 * Attempts to add a property to the event.
-	 * @param key - Name of the property.
+	 *
+	 * @param name - Name of the property.
 	 * @param value - Property value.
 	 * @return True if the property was successfully added; otherwise false.
 	 * @throws EventFrozenException If the Event has been frozen, it cannot have additional
 	 * properties set.
 	 */
-	public boolean addProperty(String key, Object value) throws EventFrozenException {
+	public boolean addProperty(String name, Object value) throws EventFrozenException {
 		if (isFrozen()) {
-			throw new EventFrozenException("Cannot add property to frozen Event.");
+			throw new EventFrozenException(EventFrozenException.CANNOT_ADD_PROPERTY);
 		}
 
-		properties.put(key, value);
+		if (!name.matches(NAME_PATTERN)) {
+			throw new MalformedNameException(MalformedNameException.BAD_PROPERTY_NAME);
+		}
+
+		properties.put(name, value);
 		return true;
 	}
 
 	/**
 	 * Gets the event property as an int.
+	 *
 	 * @param name - Name of the property.
 	 * @return The event property as an int.
+	 * @throws NullPointerException If the specified property is not found.
 	 */
 	public int getPropertyAsInt(String name) {
-		return (int)(getPropertyAsType(name, Integer.class));
+		return (int)getPropertyAsType(name, Integer.class);
 	}
 
 	/**
 	 * Gets the event property as a String.
+	 *
 	 * @param name - Name of the property.
 	 * @return The event property as a String.
+	 * @throws NullPointerException If the specified property is not found.
 	 */
 	public String getPropertyAsString(String name) {
 		return getPropertyAsType(name, String.class);
@@ -117,26 +145,21 @@ public class Event {
 
 	/**
 	 * Gets the event property as a double.
+	 *
 	 * @param name - Name of the property.
 	 * @return The event property as a double.
+	 * @throws NullPointerException If the specified property is not found.
 	 */
 	public double getPropertyAsDouble(String name) {
 		return (double)getPropertyAsType(name, Double.class);
 	}
 
 	/**
-	 * Gets the event property as a float.
-	 * @param name - Name of the property.
-	 * @return The event property as a float.
-	 */
-	public float getPropertyAsFloat(String name) {
-		return (float)getPropertyAsType(name, Float.class);
-	}
-
-	/**
 	 * Gets the event property as a boolean.
+	 *
 	 * @param name - Name of the property.
 	 * @return The event property as a boolean.
+	 * @throws NullPointerException If the specified property is not found.
 	 */
 	public boolean getPropertyAsBoolean(String name) {
 		return (boolean)getPropertyAsType(name, Boolean.class);
@@ -144,6 +167,7 @@ public class Event {
 
 	/**
 	 * Gets the event property as an Object.
+	 *
 	 * @param name - Name of the property.
 	 * @return The event property as an Object.
 	 */
@@ -153,6 +177,7 @@ public class Event {
 
 	/**
 	 * Attempts to retrieve a property cast to the specified class.
+	 *
 	 * @param <T> - Type to cast the property to.
 	 * @param name - Name of the property.
 	 * @param _class - Class to attempt to cast the property to.
---
+++ src/test/java/com/sparklicorn/bucket/util/event/TestEvent.java
@@ -0,0 +1,215 @@
+package com.sparklicorn.bucket.util.event;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.sparklicorn.bucket.util.event.Event.EventFrozenException;
+import com.sparklicorn.bucket.util.event.Event.MalformedNameException;
+
+public class TestEvent {
+
+  static final String eventName = "TEST_EVENT";
+
+  static final String[] invalidNames = new String[] {
+    "", ".", "?", "\\", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=",
+    "|", "'", "\"", ";", ":", ",", "<", ".", ">", "/", "~", "`",
+    "0mustStartWithLetter", "1meow", "666pow",
+    "_mustStartWithLetter", "-mustEhhNo", "045-___-definitelyNot",
+    "($*%&#NGFSO(DU*FYP($*NOSIUDFGHOH&*^H*O",
+    "may not contain whitespace", "dont-be\nridiculous\twith your  event\n names_69",
+    "1", "12", "123", "1234aaa"
+  };
+
+  Event event;
+
+  @BeforeEach
+  void before() {
+    event = new Event(eventName);
+  }
+
+  @Test
+  void testConstructor_setsName() {
+    assertEquals(eventName, event.name);
+  }
+
+  @Test
+  void testConstructor_whenNameIsNull_throws() {
+    assertThrows(NullPointerException.class, () -> {
+      new Event(null);
+    });
+  }
+
+  @Test
+  void testConstructor_whenNameIsInvalid_throws() {
+    for (String invalidName : invalidNames) {
+      String message = String.format(
+        "Expected event name \"%s\" to throw MalformedNameException",
+        invalidName
+      );
+
+      assertThrows(MalformedNameException.class, () -> {
+        new Event(invalidName);
+      }, message);
+    }
+  }
+
+  @Test
+  void testConstructor_whenGivenPropertyHasInvalidName_throws() {
+    for (String invalidName : invalidNames) {
+      String message = String.format(
+        "Expected property name \"%s\" to throw MalformedNameException",
+        invalidName
+      );
+
+      assertThrows(MalformedNameException.class, () -> {
+        HashMap<String, Object> props = new HashMap<>();
+        props.put(invalidName, "doesn't matter");
+        new Event(eventName, props);
+      }, message);
+    }
+  }
+
+  @Test
+  void testFreeze() {
+    assertFalse(event.isFrozen());
+    event.freeze();
+    assertTrue(event.isFrozen());
+  }
+
+  @Test
+  void testAddProperty_returnsTrue() {
+    assertTrue(event.addProperty("testProp", 1));
+  }
+
+  @Test
+  void testAddProperty_replacesExistingProperty() {
+    event.addProperty("testProp", 1);
+    event.addProperty("testProp", 2);
+    assertEquals(2, event.getPropertyAsInt("testProp"));
+  }
+
+  @Test
+  void testAddProperty_whenNameIsNull_throws() {
+    assertThrows(NullPointerException.class, () -> {
+      event.addProperty(null, "doesn't matter");
+    });
+  }
+
+  @Test
+  void testAddProperty_whenNameIsInvalid_throws() {
+    for (String invalidName : invalidNames) {
+      String message = String.format(
+        "Expected event name \"%s\" to throw MalformedNameException",
+        invalidName
+      );
+
+      assertThrows(MalformedNameException.class, () -> {
+        event.addProperty(invalidName, "meow");
+      }, message);
+    }
+  }
+
+  @Test
+  void testAddProperty_whenFrozen_throws() {
+    event.freeze();
+    assertThrows(EventFrozenException.class, () -> {
+      event.addProperty("testProp", 1);
+    });
+  }
+
+  @Test
+  void testGetProperty() {
+    event.addProperty("testProp", this);
+    assertEquals(this, event.getProperty("testProp"));
+  }
+
+  @Test
+  void testGetProperty_whenPropertyIsNotSet_returnsNull() {
+    assertNull(event.getProperty("testProp"));
+  }
+
+  @Test
+  void testGetPropertyAsType() {
+    event.addProperty("testProp", this);
+    assertEquals(this, event.getPropertyAsType("testProp", TestEvent.class));
+  }
+
+  @Test
+  void testGetPropertyAsType_whenPropertyIsNotSet_returnsNull() {
+    assertNull(event.getPropertyAsType("testProp", TestEvent.class));
+  }
+
+  @Test
+  void testGetPropertyAsType_whenPropertyIsNull_returnsNull() {
+    assertNull(event.getPropertyAsType("testProp", TestEvent.class));
+  }
+
+  @Test
+  void testGetPropertyAsType_whenPropertyIsNotGivenType_throws() {
+    event.addProperty("testProp", 9);
+    assertThrows(ClassCastException.class, () -> {
+      event.getPropertyAsType("testProp", TestEvent.class);
+    });
+  }
+
+  @Test
+  void testGetPropertyMethods() {
+    event.addProperty("intProp", 11);
+    event.addProperty("stringProp", "meow");
+    event.addProperty("doubleProp", Double.MAX_VALUE);
+    event.addProperty("booleanProp", true);
+
+    assertEquals(11, event.getPropertyAsInt("intProp"));
+    assertEquals("meow", event.getPropertyAsString("stringProp"));
+    assertEquals(Double.MAX_VALUE, event.getPropertyAsDouble("doubleProp"));
+    assertEquals(true, event.getPropertyAsBoolean("booleanProp"));
+  }
+
+  @Test
+  void testGetPropertyAsInt_whenPropertyIsNotSet_throws() {
+    assertThrows(NullPointerException.class, () -> {
+      event.getPropertyAsInt("testProp");
+    });
+  }
+
+  @Test
+  void testGetPropertyAsString_whenPropertyIsNotSet_throws() {
+    assertThrows(NullPointerException.class, () -> {
+      event.getPropertyAsInt("testProp");
+    });
+  }
+
+  @Test
+  void testGetPropertyAsDouble_whenPropertyIsNotSet_throws() {
+    assertThrows(NullPointerException.class, () -> {
+      event.getPropertyAsInt("testProp");
+    });
+  }
+
+  @Test
+  void testGetPropertyAsBoolean_whenPropertyIsNotSet_throws() {
+    assertThrows(NullPointerException.class, () -> {
+      event.getPropertyAsInt("testProp");
+    });
+  }
+
+  // Integration Tests
+
+  @Test
+  void testConstructor_setsProperties() {
+    HashMap<String,Object> props = new HashMap<>();
+    props.put("testProp1", "apple");
+    props.put("testProp2", 96);
+    props.put("testProp3", props);
+
+    event = new Event(eventName, props);
+
+    assertEquals("apple", event.getPropertyAsString("testProp1"));
+    assertEquals(96, event.getPropertyAsInt("testProp2"));
+    assertEquals(props, event.getPropertyAsType("testProp3", HashMap.class));
+  }
+}
