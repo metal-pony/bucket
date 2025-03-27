3fd7f872af0cd86657aaae47fb4119a370c9ea96
Jeff <metal-pony@users.noreply.github.com>
2025-03-27 14:10:59 -0400

    ThreadPool better control of num threads

--- bucket/src/main/java/com/metal_pony/bucket/util/ThreadPool.java
+++ bucket/src/main/java/com/metal_pony/bucket/util/ThreadPool.java
@@ -10,8 +10,8 @@ import java.util.concurrent.TimeUnit;
 
 public class ThreadPool {
 
-	private static final long DEFAULT_TIMEOUT = 1L;
-	private static final TimeUnit DEFAULT_TIMEOUT_UNIT = TimeUnit.MINUTES;
+	private static final long CORE_THREAD_MAX_IDLE_TIME = 25L;
+	private static final TimeUnit CORE_THREAD_MAX_IDLE_TIME_UNIT = TimeUnit.MILLISECONDS;
 
 	private static int coreThreads;
 	private static int maxThreads;
@@ -25,16 +25,46 @@ public class ThreadPool {
 
 		int numProcessors = Runtime.getRuntime().availableProcessors();
 
-		coreThreads = numProcessors <= 4 ? 1 : numProcessors / 4;
-		maxThreads = numProcessors;
+		coreThreads = numProcessors <= 4 ? 2 : numProcessors / 4;
+		maxThreads = numProcessors / 2;
 		workQueue = new LinkedBlockingQueue<Runnable>();
 		pool = new ThreadPoolExecutor(
 			coreThreads,
 			maxThreads,
-			DEFAULT_TIMEOUT,
-			DEFAULT_TIMEOUT_UNIT,
+			CORE_THREAD_MAX_IDLE_TIME,
+			CORE_THREAD_MAX_IDLE_TIME_UNIT,
 			workQueue
 		);
+	}
+
+	public static synchronized void setCoreThreads(int coreThreads) {
+		if (coreThreads < 1) {
+			throw new IllegalArgumentException(String.format("coreThreads (%d) too low", coreThreads));
+		}
+		init();
+		pool.setCorePoolSize(coreThreads);
+	}
+
+	public static synchronized void setMaxThreads(int maxThreads) {
+		if (maxThreads < 1) {
+			throw new IllegalArgumentException(String.format("maxThreads (%d) too low", maxThreads));
+		}
+		init();
+		pool.setMaximumPoolSize(maxThreads);
+	}
+
+	public static synchronized void useMaxThreads() {
+		init();
+		pool.setMaximumPoolSize(Runtime.getRuntime().availableProcessors() - 1);
+		pool.setCorePoolSize(Runtime.getRuntime().availableProcessors() - 1);
+		pool.prestartAllCoreThreads();
+	}
+
+	public static synchronized void setSizeAndStart(int poolSize) {
+		init();
+		pool.setMaximumPoolSize(poolSize);
+		pool.setCorePoolSize(poolSize);
+		pool.prestartAllCoreThreads();
 	}
 
 	public static synchronized void setKeepAliveTime(long time, TimeUnit unit) {
